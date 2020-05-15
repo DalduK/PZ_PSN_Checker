376e6886b0e1a7ec89b0b9f87bf8a5de37b7dee8
Andrzej <skrobak.andrzej@gmail.com>
2020-05-16 01:50:12 +0200

    Rejestracja użytkownikow, rozbudowanie bazy danych

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Item)
+admin.site.register(ItemPrice)
+admin.site.register(Basket)
+admin.site.register(BasketItem)
